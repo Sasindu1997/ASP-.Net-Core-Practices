13f0581999f3e2978934bb3afac6e15fe05c8db1
Sasindu1997 <sasinduusgalhewa@gmail.com>
2022-09-01 21:20:10 +0530

    Add New Book to Book List

--- Razor-BookList/Program.cs
+++ Razor-BookList/Program.cs
@@ -12,7 +12,7 @@ namespace Razor_BookList
     public class Program
     {
         public static void Main(string[] args)
-        {
+        { 
             CreateHostBuilder(args).Build().Run();
         }
 
